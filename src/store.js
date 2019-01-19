10383f08a1aeb30ffaad540fd1501538e99a4ce7
wojtekjPJATK <wojtekj1989@gmail.com>
2019-01-19 19:55:03 +0100

    added Auth routes and api calls

--- src/store.js
+++ src/store.js
@@ -1,16 +1,35 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import api from "@/api";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    session: localStorage.getItem("session")
+  },
+  getters: {
+    session(state) {
+      return state.session;
+    }
   },
   mutations: {
-
+    session(state, id) {
+      state.session = id;
+    },
+    deleteSession(state) {
+      state.session = null;
+    }
   },
   actions: {
-
+    logout(context) {
+      return api.logout(context);
+    },
+    join(context, data) {
+      return api.join(context, data);
+    },
+    signin(context, data) {
+      return api.signin(context, data);
+    }
   }
-})
+});
