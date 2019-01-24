4b83cc29e258fb589ec86a3dc5d4279d8b787fc9
wojtekjPJATK <wojtekj1989@gmail.com>
2019-01-24 05:54:10 +0100

    Bunker data refactor

---
+++ src/game/bunker.js
@@ -0,0 +1,87 @@
+export default {
+  buildings: [
+    {
+      name: "Armory",
+      url: "https://storage.googleapis.com/arc-mmo-game/buildings/ammo.png",
+      level: "",
+      income: "",
+      upgrade: {
+        caps: "",
+        wood: "",
+        points: ""
+      }
+    },
+    {
+      name: "Bank",
+      url: "https://storage.googleapis.com/arc-mmo-game/buildings/bank.png",
+      level: "",
+      income: "",
+      upgrade: {
+        caps: "",
+        wood: "",
+        points: ""
+      }
+    },
+    {
+      name: "Bunker",
+      url: "https://storage.googleapis.com/arc-mmo-game/buildings/bunkier.png",
+      level: "",
+      income: "",
+      upgrade: {
+        caps: "",
+        wood: "",
+        points: ""
+      }
+    },
+    {
+      name: "Pantry",
+      url: "https://storage.googleapis.com/arc-mmo-game/buildings/pantry.png",
+      level: "",
+      income: "",
+      upgrade: {
+        caps: "",
+        wood: "",
+        points: ""
+      }
+    },
+    {
+      name: "Lumber Mill",
+      url: "https://storage.googleapis.com/arc-mmo-game/buildings/tartak.png",
+      level: "",
+      income: "",
+      upgrade: {
+        caps: "",
+        wood: "",
+        points: ""
+      }
+    }
+  ],
+  resources: [
+    {
+      name: "Wood",
+      url: "https://storage.googleapis.com/arc-mmo-game/resources/deski.png",
+      amount: ""
+    },
+    {
+      name: "Caps",
+      url: "https://storage.googleapis.com/arc-mmo-game/resources/kapsle.png",
+      amount: ""
+    },
+    {
+      name: "Ammo",
+      url: "https://storage.googleapis.com/arc-mmo-game/resources/naboje.png",
+      amount: ""
+    },
+    {
+      name: "Berries",
+      url:
+        "https://storage.googleapis.com/arc-mmo-game/resources/zmutowanejagody.png",
+      amount: ""
+    },
+    {
+      name: "Action Points",
+      url: "https://storage.googleapis.com/arc-mmo-game/resources/actions.png",
+      amount: ""
+    }
+  ]
+};
